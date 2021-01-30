c378d26fb62b6bd78cf400d784de07f9e96c1815
Francisco Molina <femolina@uc.cl>
2021-01-30 15:09:46 +0100

    Dockerfile: add HTTP_HOST/PORT to run.sh

--- run.sh
+++ run.sh
@@ -2,8 +2,11 @@
 
 : ${COAP_HOST:=[::1]}
 : ${COAP_PORT:=5683}
+: ${HTTP_HOST:=localhost}
+: ${HTTP_PORT:=8080}
 
 python3 /opt/ota-server/otaserver/main.py \
             --coap-host=${COAP_HOST} --coap-port=${COAP_PORT} \
-            --http-port=8080 \
+            --http-host=${HTTP_HOST} \
+            --http-port=${HTTP_PORT} \
             --debug
